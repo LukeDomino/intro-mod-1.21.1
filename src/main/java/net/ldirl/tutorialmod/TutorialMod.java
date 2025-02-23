a1bad451d1f6ad51dcbb4c85953197ce2b403fc0
LukeDomino <luke1234v@gmail.com>
2025-02-22 16:50:47 -0800

    Update 1

--- src/main/java/net/ldirl/tutorialmod/TutorialMod.java
+++ src/main/java/net/ldirl/tutorialmod/TutorialMod.java
@@ -2,16 +2,20 @@ package net.ldirl.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.ldirl.tutorialmod.block.ModBlocks;
+import net.ldirl.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
-	public static final String MOD_ID = "tutorial-mod";
+	public static final String MOD_ID = "tutorialmod";
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 
 	}
 }
